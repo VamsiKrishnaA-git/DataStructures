b5ef9ff7309097c4bd02e8ebd9071dcf5480da86
VamsiKrishnaA-git <vamsikrishnaathota@outlook.com>
2020-06-14 14:27:39 -0500

    Initial Commit.

--- src/Main.java
+++ src/Main.java
@@ -14,18 +14,26 @@ public class Main {
         //obj.deleteAtHead();
         //obj.deleteByVal(58888);
         Stack obj1 = new Stack(5);
-        System.out.println(obj1.isEmpty());
-        obj1.push(8);
-        obj1.push(9);
-        obj1.push(12);
-        obj1.push(128);
-        obj1.push(111);
-        obj1.push(12);
+        //System.out.println(obj1.isEmpty());
+        //obj1.push(8);
+        //obj1.push(9);
+        //obj1.push(12);
+        //obj1.push(128);
+        //obj1.push(111);
+        //obj1.push(12);
         //obj1.pop();
         //obj1.pop();
-        System.out.println(obj1.pop());
-        System.out.println(obj1.top);
-        obj1.show();
+        CircularQueue obj2 = new CircularQueue(5);
+        obj2.enqueue(5);
+        obj2.enqueue(6);
+        obj2.enqueue(8);
+        //System.out.println(obj2.dequeue());
+        obj2.dequeue();
+        obj2.dequeue();
+        obj2.dequeue();
+       // System.out.println(obj1.pop());
+        //System.out.println(obj1.top);
+        obj2.show();
 
     }
 
