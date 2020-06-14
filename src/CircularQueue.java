2950aa7b63cdc423ae9dc4c8b8e5d6dbd677afd0
VamsiKrishnaA-git <vamsikrishnaathota@outlook.com>
2020-06-14 14:30:17 -0500

    Revert: Initial circular queue Commit.

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -1,2 +1,53 @@
 public class CircularQueue<V> {
+    private int max_size;
+    private int front;
+    private int rear;
+    private int currSize;
+    private V[] array;
+
+    public CircularQueue(int max_size){
+        this.max_size = max_size;
+        array = (V[])new Object[max_size];
+        front = 0;
+        rear = -1;
+        currSize = 0;
+    }
+
+    public boolean isEmpty(){
+        return currSize == 0;
+    }
+
+    public boolean isFull(){
+        return currSize == max_size;
+    }
+
+    public V top(){
+        if(isEmpty())
+            return null;
+        return array[currSize];
+    }
+
+    public void enqueue(V data){
+        if(isFull())
+            return;
+        rear = (rear + 1)%max_size;
+        array[rear] = data;
+        currSize++;
+
+    }
+
+    public V dequeue(){
+        if(isEmpty())
+            return null;
+        V temp = array[front];
+        front = (front + 1)%max_size;
+        currSize--;
+        return temp;
+    }
+
+    public void show(){
+        for(int i = front;i< currSize;i++){
+            System.out.println((array[(i)%max_size]));
+        }
+    }
 }
