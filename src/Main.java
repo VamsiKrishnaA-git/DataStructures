76f2a323ce8077e663298315e8e45e68d6ba5fe8
VamsiKrishnaA-git <vamsikrishnaathota@outlook.com>
2020-06-14 13:16:39 -0500

    Initial Commit.

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,29 @@ public class Main {
         SinglyLinkedList obj = new SinglyLinkedList();
         System.out.println(obj.isEmpty());
        // obj.insertAtHead(2);
-        obj.insertAtHead(5);
-        obj.insertAtHead(8);
+        //obj.insertAtHead(5);
+       // obj.insertAtHead(8);
         //obj.insertAtAfter(2,8);
         //obj.insertAtAfter(10,5);
-        obj.insertAtEnd(15);
-        obj.insertAtEnd(121);
+        //obj.insertAtEnd(15);
+        //obj.insertAtEnd(121);
         //obj.deleteAtEnd();
         //obj.deleteAtHead();
-        obj.deleteAtHead();
-        obj.deleteByVal(58888);
-        System.out.println(obj.searchElement(5455));
-        obj.show();
+        //obj.deleteAtHead();
+        //obj.deleteByVal(58888);
+        Stack obj1 = new Stack(5);
+        System.out.println(obj1.isEmpty());
+        obj1.push(8);
+        obj1.push(9);
+        obj1.push(12);
+        obj1.push(128);
+        obj1.push(111);
+        obj1.push(12);
+        //obj1.pop();
+        //obj1.pop();
+        System.out.println(obj1.pop());
+        System.out.println(obj1.top);
+        obj1.show();
 
     }
 
--- src/Stack.java
+++ src/Stack.java
@@ -5,6 +5,44 @@ public class Stack<V> {
 
     public Stack(int max_size){
         this.max_size = max_size;
-        array = (v[])new Object(array);
+        array = (V[])new Object[max_size];
+        top = -1;
     }
+
+    public boolean isEmpty(){
+        return top == -1;
+    }
+
+    public boolean isFull(){
+        return top == max_size-1;
+    }
+
+    public V top(){
+        if(isEmpty())
+            return null;
+        return array[top];
+    }
+
+    public void push(V data){
+        if(isFull())
+            return;
+        array[++top] = data;
+    }
+
+    public V pop(){
+        if(isEmpty())
+            return null;
+        return array[top--];
+    }
+
+    public void show(){
+        while(top!=-1){
+            System.out.println(array[top]);
+            top--;
+        }
+    }
+
+
+
+
 }
